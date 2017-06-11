8a4f13c27d105dddc9936ed4190bbbabf7d52b9e
Rishabh Pandita <rishabh.pandita@gmail.com>
2017-06-10 20:31:05 -0400

    UI for the application initial cut

---
+++ app/src/main/java/com/pandita/rishabh/androidservicesshutdown/MainActivity.java
@@ -0,0 +1,21 @@
+package com.pandita.rishabh.androidservicesshutdown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+}
